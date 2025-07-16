1143a399a9e5c9f2ce2a0bc538e8313447236fa1
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-16 12:08:09 +0330

    SeedData in Test Layer

--- AdventureWorks.Tests/Repositories/ProductRepositoryTests.cs
+++ AdventureWorks.Tests/Repositories/ProductRepositoryTests.cs
@@ -1,7 +1,52 @@
+using AdventureWorks.Domain.Entities;
+using AdventureWorks.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace AdventureWorks.Tests.Repositories
 {
     public class ProductRepositoryTests
     {
+        private ApplicationDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            var category = new ProductCategory
+            {
+                ProductCategoryID = 1,
+                Name = "Bikes",
+            };
+
+            var subcategory = new ProductSubcategory
+            {
+                ProductSubcategoryID = 1,
+                Name = "Mountain Bikes",
+                ProductCategoryID = 1,
+                ProductCategory = category,
+            };
+
+            var product = new Product
+            {
+                ProductID = 1,
+                Name = "Trail Bike",
+                ListPrice = 1000,
+                ProductSubcategoryID = 1,
+                ProductSubcategory = subcategory,
+            };
+
+            context.ProductCategories.Add(category);
+            context.ProductSubcategories.Add(subcategory);
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return context;
+        }
+
+
+        [Fact]
         
     }
 }
