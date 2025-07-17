f20842e1fcb1328a235603d07611dd58147576ae
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-17 22:27:24 +0330

    Add unit test for GetAllProductsQueryHandler using Moq

--- AdventureWorks.Tests/Handlers/GetAllProductsQueryHandlerTests.cs
+++ AdventureWorks.Tests/Handlers/GetAllProductsQueryHandlerTests.cs
@@ -1,7 +1,46 @@
+using AdventureWorks.Application.Features.Products.Queries.GetAllProducts;
+using AdventureWorks.Application.Interfaces;
+using AdventureWorks.Domain.Entities;
+using FluentAssertions;
+using Moq;
+
 namespace AdventureWorks.Tests.Handlers
 {
     public class GetAllProductsQueryHandlerTests
     {
-        
+        [Fact]
+        public async Task Handle_ShouldReturnMappedProductDtos()
+        {
+            var mockRepo = new Mock<IProductRepository>();
+
+            var sampleProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Trail Bike",
+                ListPrice = 1000,
+                ProductSubcategory = new ProductSubcategory
+                {
+                    Name = "Mountain Bikes",
+                    ProductCategory = new ProductCategory
+                    {
+                        Name = "Bikes"
+                    }
+                }
+            };
+
+            mockRepo.Setup(r => r.GetAllWithCategoryAsync())
+            .ReturnsAsync(new List<Product> { sampleProduct });
+
+            var handler = new GetAllProductsQueryHandler(mockRepo.Object);
+            var query = new GetAllProductsQuery();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Should().NotBeNullOrEmpty();
+            result[0].Name.Should().Be("Trail Bike");
+            result[0].Price.Should().Be(1000);
+            result[0].Category.Should().Be("Bikes");
+            result[0].Subcategory.Should().Be("Mountain Bikes");
+        }
     }
 }
