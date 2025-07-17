b1687724149461d6469dbed9d4bad2ecaa28b850
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-18 00:06:50 +0330

    Add ProductsController with GET endpoint using MediatR

---
+++ AdventureWorks.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+using AdventureWorks.Application.Features.Products.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllProductsQuery());
+            return Ok(result);
+        }
+    }
+}
