aae9d763d814b30ca0a268a9c3152e3189b46cda
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-21 09:06:36 +0330

    Created Person and Customer Entity

---
+++ AdventureWorks.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.Domain.Entities
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }//PK
+        public int? PersonID { get; set; }//FK 
+
+        public Person? Person { get; set; }
+        public ICollection<SalesOrderHeader> Orders { get; set; }
+    }
+}
